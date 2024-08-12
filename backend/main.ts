0363837660666172086c60ea943f5150b5a655dc
shadinmd <shadnmhd97@gmail.com>
2024-08-12 21:17:44 +0530

    update: boiler plate code

---
+++ backend/main.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import { loadEnvFile } from "process"
+
+loadEnvFile(".env")
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(cors({
+	origin: "*"
+}))
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+	console.log(`server started listening http://localhost:${PORT}`)
+})
