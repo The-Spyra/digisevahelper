8c855ee8ef3a7165d40babad4d2c6257e48f717e
shadinmd <shadinmhd97@gmail>
2024-08-15 10:58:53 +0530

    update: unverfieid user details rotue

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,8 +1,15 @@
 import { Router } from "express"
-import { login, register } from "../controllers/auth.controller"
+import {
+  login,
+  register,
+  unverifiedUserDetails,
+  verify,
+} from "../controllers/auth.controller"
 const authRoute = Router()
 
 authRoute.post("/login", login)
 authRoute.post("/register", register)
+authRoute.post("/verify", verify)
+authRoute.post("/unverifiedUser", unverifiedUserDetails)
 
 export default authRoute
