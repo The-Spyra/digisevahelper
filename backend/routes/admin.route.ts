a624e58efdc2c9d9936e30c6ff3c66cf58a74cc8
shadinmd <shadinmhd97@gmail>
2024-08-17 21:49:41 +0530

    fix: missing admin routes

--- backend/routes/admin.route.ts
+++ backend/routes/admin.route.ts
@@ -1,9 +1,13 @@
 import { Router } from "express"
 import adminToolRoute from "./admin/tool.route"
+import adminFormRoute from "./admin/form.route"
+import adminPosterRoute from "./admin/poster.route"
 import adminAuthRoute from "./admin/auth.route"
 import adminAuthorizationMiddleware from "../middlewares/adminauthorization.middleware"
 const adminRoute = Router()
 
 adminRoute.use("/auth", adminAuthRoute)
 adminRoute.use("/tool", adminAuthorizationMiddleware, adminToolRoute)
+adminRoute.use("/form", adminAuthorizationMiddleware, adminFormRoute)
+adminRoute.use("/poster", adminAuthorizationMiddleware, adminPosterRoute)
 export default adminRoute
