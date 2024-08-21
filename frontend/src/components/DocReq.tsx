d8cbcc3a016cd68c5077d714ba07ffff396cac55
shadinmd <shadinmhd97@gmail>
2024-08-21 16:41:33 +0530

    fix: no key props for rendering list

--- frontend/src/components/DocReq.tsx
+++ frontend/src/components/DocReq.tsx
@@ -1,24 +1,23 @@
-import React from "react";
-
 interface Props {
-  title: string;
-  docs: string[];
+  title: string
+  docs: string[]
 }
 
 function DocReq({ title, docs }: Props) {
   return (
     <div className=" bg-[#9B9E9E80] rounded-xl">
-      <h1 className="bg-[#9B9E9E80] text-[32px] font-extrabold text-center rounded-t-xl">{title}</h1>
+      <h1 className="bg-[#9B9E9E80] text-[32px] font-extrabold text-center rounded-t-xl">
+        {title}
+      </h1>
       <div className="p-2">
-
-      <ul className="list-decimal text-[20px] font-[500] list-inside">
-        {docs.map((x) => (
-            <li>{x}</li>
-        ))}
-      </ul>
-        </div>
+        <ul className="list-decimal text-[20px] font-[500] list-inside">
+          {docs.map((x) => (
+            <li key={x}>{x}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default DocReq;
+export default DocReq
