19131390369bc6afa0bbb3b190cc1692abb5c8ba
shadinmd <shadinmhd97@gmail>
2024-08-30 16:06:25 +0530

    update: changed to gmail and new verification  mail template

--- backend/utils/mail.ts
+++ backend/utils/mail.ts
@@ -3,18 +3,15 @@ import { loadEnvFile } from "process"
 
 loadEnvFile(".env")
 
-const EMAIL_SERVER = process.env.EMAIL_SERVER
-const EMAIL_PORT = process.env.EMAIL_PORT
 const EMAIL = process.env.EMAIL
 const EMAIL_PASS = process.env.EMAIL_PASS
 
-if (!EMAIL || !EMAIL_SERVER || !EMAIL_PORT || !EMAIL_PASS) {
+if (!EMAIL || !EMAIL_PASS) {
   throw new Error("Missing enironment variables")
 }
 
 const transporter = nodeMailer.createTransport({
-  host: EMAIL_SERVER,
-  port: Number(EMAIL_PORT),
+  service: "gmail",
   auth: {
     user: EMAIL,
     pass: EMAIL_PASS,
@@ -26,7 +23,54 @@ export const sendVeirificationMail = async (to: string, url: string) => {
     from: EMAIL,
     to,
     subject: "DIGISEVAHELPER account verification link",
-    text: `opt link: ${url}`,
+    html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Please activate your account</title>
+  <!--[if mso]><style type="text/css">body, table, td, a { font-family: Arial, Helvetica, sans-serif !important; }</style><![endif]-->
+</head>
+
+<body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+  <table role="presentation"
+    style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; background-color: rgb(239, 239, 239);">
+    <tbody>
+      <tr>
+        <td align="center" style="padding: 1rem 2rem; vertical-align: top; width: 100%;">
+          <table role="presentation" style="max-width: 600px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left;">
+            <tbody>
+              <tr>
+                <td style="padding: 40px 0px 0px;">
+                  <div style="text-align: left;">
+                    <!---->
+                  </div>
+                  <div style="padding: 20px; background-color: rgb(255, 255, 255);">
+                    <div style="color: rgb(0, 0, 0); text-align: left;">
+                      <h1 style="margin: 1rem 0">Final step...</h1>
+                      <p style="padding-bottom: 16px">Follow this link to verify your email address.</p>
+                      <p style="padding-bottom: 16px"><a href="${url}" target="_blank"
+                          style="padding: 12px 24px; border-radius: 4px; color: #FFF; background: #2aa88f;display: inline-block;margin: 0.5rem 0;">Confirm
+                          now</a></p>
+                      <p style="padding-bottom: 16px">If you didn’t ask to verify this address, you can ignore this email.</p>
+                      <p style="padding-bottom: 16px">Thanks,<br>The DIGISEVAHELPER team</p>
+                    </div>
+                  </div>
+                  <div style="padding-top: 20px; color: rgb(153, 153, 153); text-align: center;">
+                    <p style="padding-bottom: 16px">Made with ♥ in india</p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+
+</html>`,
   })
 }
 
